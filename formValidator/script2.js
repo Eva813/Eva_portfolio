d8a3faf16ce139e07e0c1d94023a0c0ba0156160
Eva813 <as45986@gmail.com>
2021-01-10 23:44:02 +0800

    add password validation

--- formValidator/script2.js
+++ formValidator/script2.js
@@ -52,3 +52,33 @@ $("#username").keyup(function () {
   }
 
 })
+
+//正確時，外框顯示綠色，錯誤時紅色 
+
+
+//密碼
+$("#password").keyup(function (e) {
+
+  e.preventDefault();
+  var password_input = $("#password").val();
+  var message_password = $("#password").siblings('small').first();
+  if (password_input.length < 8) {
+
+    $(this).css('border-color', '#e74c3c');
+
+    message_password.text("Password must be at least 8 characters").css('visibility', 'visible');
+  } else if (password_input.length > 16) {
+
+    $(this).css('border-color', '#e74c3c');
+
+    message_password.text("Password must be at less than 16 characters").css('visibility', 'visible');
+  } else {
+
+    $(this).css('border-color', '#2ecc71');
+
+
+    message_password.css('visibility', 'hidden')
+  }
+
+
+})
