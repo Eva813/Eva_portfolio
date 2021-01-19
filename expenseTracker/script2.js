eeaf5725b94e973949e92646f08361002ff552d8
Eva813 <as45986@gmail.com>
2021-01-19 14:15:55 +0800

    add transaction and clear form

--- expenseTracker/script2.js
+++ expenseTracker/script2.js
@@ -11,4 +11,10 @@ $(document).ready(function () {
 function addTransactions(name, amount) {
   var Transaction_str = `<li class='plus' >${name}<span> ${amount}</span><button class="delete-btn">x</button></li>`
   $('#list').append(Transaction_str);
+
+  $('#text').val('');
+  $('#amount').val('');
 }
+
+
+//在表格中輸入值，交出後清空
