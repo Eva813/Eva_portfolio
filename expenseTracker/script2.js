52d38b50e0506e7267f267743ff21fa480c993f2
Eva813 <as45986@gmail.com>
2021-01-19 14:04:34 +0800

    add transaction to list

--- expenseTracker/script2.js
+++ expenseTracker/script2.js
@@ -2,5 +2,13 @@ $(document).ready(function () {
   $('.btn').click(function (e) {
     e.preventDefault();
     console.log('click');
+    addTransactions('cash', '+200');
   })
 });
+
+
+//在list中插入
+function addTransactions(name, amount) {
+  var Transaction_str = `<li class='plus' >${name}<span> ${amount}</span><button class="delete-btn">x</button></li>`
+  $('#list').append(Transaction_str);
+}
