eb708d4011c21f9e33f6dcc1347736401a08f89a
Eva813 <as45986@gmail.com>
2021-01-23 23:15:35 +0800

    play the audio

--- musicPlayer/script.js
+++ musicPlayer/script.js
@@ -1,12 +1,15 @@
 $(document).ready(function (e) {
 
   $('#play').click(function (e) {
+    var hasPlay = $('.music-countainer').hasClass('paly');
 
-
-
-
-
+    if ($('#play').paused) {
+      pauseMusic();
+    } else {
+      playMusic();
+    }
   });
+
 });
 
 
@@ -17,14 +20,15 @@ function playMusic() {
   $('#play').find('i').removeClass('fa-play');
   $('#play').find('i').addClass('fa-pause');
 
+  $('#audio').get(0).play();
 };
 //停止音樂
 function pauseMusic() {
   $('.music-container').removeClass('play');
   $('#play').find('i').addClass('fa-play');
   $('#play').find('i').removeClass('fa-pause');
+  $('#audio').get(0).pause();
 }
 
+//使音樂出現
 
-var a = $('.music-container').hasClass('play');
-console.log(a)
