e2500b3d671f13be0a667400a85d3d1f8b2b346b
Eva813 <as45986@gmail.com>
2021-02-12 21:09:49 +0800

    get the result of speech recognition

--- guessNumber/script.js
+++ guessNumber/script.js
@@ -9,11 +9,31 @@ let randomNum = theRandomNum();
 console.log(randomNum);
 
 //語音辨識
-window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+// window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
 
-let recognition = new window.SpeechRecognition();
+// let recognition = new window.SpeechRecognition();
+
+try {
+  var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+}
+catch (e) {
+  console.error(e);
+
+}
+
+
 
 
 // 開始語音辨識
 recognition.start();
 //取得語音辨識的結果
+recognition.onresult = function (event) {
+  console.log(event);
+  let myWord = event.results[0][0].transcript
+  console.log('You said: ', myWord);
+};
+
+
+
+
