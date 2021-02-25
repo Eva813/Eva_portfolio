f537672b88cf0e37c66e5663175d945471e92702
Eva813 <as45986@gmail.com>
2021-02-25 22:33:58 +0800

    insert user profile to html

--- githubFinder/script.js
+++ githubFinder/script.js
@@ -17,8 +17,33 @@ function getGithubUser(username) {
     type: "get",
     url: url,
     dataType: "json",
-    success: function (response) {
-      console.log(response);
+    success: function (data) {
+      insertProfile(data);
     }
   });
+}
+
+function insertProfile(user) {
+
+  let words = `<div class="row">
+          <div class="col-md-3">
+            <img class="img-fluid mb-2" src="${user.avatar_url}">
+            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+          </div>
+          <div class="col-md-9">
+            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+            <span class="badge badge-success">Followers: ${user.followers}</span>
+            <span class="badge badge-info">Following: ${user.following}</span>
+            <br><br>
+            <ul class="list-group">
+              <li class="list-group-item">Company: ${user.company}</li>
+              <li class="list-group-item">Website/Blog: ${user.blog} </li>
+              <li class="list-group-item">Location: ${user.location}</li>
+              <li class="list-group-item">Member Since: ${user.created_at}</li>
+            </ul>
+          </div>
+        </div>`
+
+  $('#profile').html(words);
 }
