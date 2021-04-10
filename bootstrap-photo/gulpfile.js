6c947f9728b7f998a5a5be9bb83837bbc7fba62b
Eva813 <as45986@gmail.com>
2021-04-10 09:25:43 +0800

    add npm file

--- bootstrap-photo/gulpfile.js
+++ bootstrap-photo/gulpfile.js
@@ -9,41 +9,41 @@ var uglify = require('gulp-uglify-es').default;
 
 
 
-gulp.task('scripts', function() {
-  return gulp.src([
-    /* Add your JS files here, they will be combined in this order */
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/popper.js/dist/umd/popper.js',
-    'node_modules/bootstrap/js/dist/util.js',
-    'node_modules/bootstrap/js/dist/alert.js',
-    'node_modules/bootstrap/js/dist/button.js',
-    'node_modules/bootstrap/js/dist/dropdown.js',
-    'node_modules/bootstrap/js/dist/tab.js',
-    'node_modules/bootstrap/js/dist/carousel.js',
-    'node_modules/bootstrap/js/dist/collapse.js',
-    'node_modules/bootstrap/js/dist/modal.js',
-    'node_modules/bootstrap/js/dist/scrollspy.js',
-    'node_modules/bootstrap/js/dist/tooltip.js',
-    'node_modules/bootstrap/js/dist/popover.js',
-    'node_modules/bootstrap/js/dist/toast.js',
+gulp.task('scripts', function () {
+    return gulp.src([
+        /* Add your JS files here, they will be combined in this order */
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/popper.js/dist/umd/popper.js',
+        'node_modules/bootstrap/js/dist/util.js',
+        'node_modules/bootstrap/js/dist/alert.js',
+        'node_modules/bootstrap/js/dist/button.js',
+        'node_modules/bootstrap/js/dist/dropdown.js',
+        'node_modules/bootstrap/js/dist/tab.js',
+        'node_modules/bootstrap/js/dist/carousel.js',
+        'node_modules/bootstrap/js/dist/collapse.js',
+        'node_modules/bootstrap/js/dist/modal.js',
+        'node_modules/bootstrap/js/dist/scrollspy.js',
+        'node_modules/bootstrap/js/dist/tooltip.js',
+        'node_modules/bootstrap/js/dist/popover.js',
+        'node_modules/bootstrap/js/dist/toast.js',
 
 
 
-    'src/js/main.js',
-    'src/js/other.js'
+        'src/js/main.js',
+        'src/js/other.js'
 
 
     ])
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('./js'));
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./js'));
 });
 
 gulp.task('sass', function () {
     gulp.src('src/scss/main.scss')
-        .pipe(sass({includePaths: ['scss']}))
+        .pipe(sass({ includePaths: ['scss'] }))
         .pipe(gulp.dest('./css'))
         .pipe(minifycss());
 });
@@ -51,7 +51,7 @@ gulp.task('html', function () {
     gulp.src('./*.html')
         .pipe(gulp.dest('./'));
 });
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init(["css/*.css", "js/*.js"], {
         server: {
             baseDir: "./"
@@ -59,8 +59,15 @@ gulp.task('browser-sync', function() {
     });
 });
 
+// //Deploy to ghPages Task
+// gulp.task('ghpages', ['build'], function () {
+//     return gulp.src(dirs.dest + '/**/*')
+//         .pipe(ghPages());
+// });
+
 gulp.task('default', ['sass', 'browser-sync'], function () {
     gulp.watch("src/scss/**/*.scss", ['sass']).on("change", reload);
     gulp.watch("./*.html", ['html']).on("change", reload);
     gulp.watch("src/js/*.js", ['scripts']).on("change", reload);
 });
+
