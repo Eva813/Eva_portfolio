e657df83eb8d63497627cea72e5b5de49a63e26a
Eva813 <as45986@gmail.com>
2021-05-15 13:18:45 +0800

    start script file

---
+++ ArrayMethods/script.js
@@ -0,0 +1,16 @@
+
+//先抓取需要的資料
+const main = document.getElementById('main');
+//取得按鈕的id
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const millionBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calBtn = document.getElementById('calculate-wealth');
+
+//array 的初始化
+let data = [];
+
+//取得使用者和錢
+//使用fetch來取得api
+
